fd66400b7db3c6da5e26c15bc4e222177467010b
Mixberry <keriokea@gmail.com>
2020-02-25 14:58:11 +0700

    small correct

--- lab2/prog.c
+++ lab2/prog.c
@@ -13,12 +13,14 @@ int main(int argc, char *argv[])
 {
     char dirname[MAXDIR];
     int max_num_of_links = 0;
+    //get current working directory
     if(getcwd(dirname, MAXDIR) == NULL)
     {
 		perror("getcwd() ");
 		return errno;
 	}
 	
+	//open current catalog
 	DIR *dir;
     if((dir = opendir(dirname)) == NULL)
     {
@@ -28,9 +30,14 @@ int main(int argc, char *argv[])
 
     struct dirent *dirstr;
     struct stat stbuf;
-
+    
+    //get max num of links in 
+    //this catalog
+    
+	//reading all catalog's files
     while((dirstr = readdir(dir)) != NULL)
     {
+		//get stat for current catalog's file
         if(stat(dirstr->d_name, &stbuf) == -1)
         {
             perror("stat() ");
@@ -44,15 +51,19 @@ int main(int argc, char *argv[])
 			}
 		}
 	}
-
+	//pointer get first catalog file link
     rewinddir(dir);
-
+	
+	//reading all files with
+	//diff num of links
     for(int i = max_num_of_links; i > 0; i--)
     {
         printf("====================\n");
         printf("Number of links: %d\n", i);
+        //reading all catalog's files
         while((dirstr = readdir(dir)) != NULL)
         {
+			//get stat for current catalog's file
             if(stat(dirstr->d_name, &stbuf) == -1)
             {
                 perror("stat() ");
@@ -60,6 +71,8 @@ int main(int argc, char *argv[])
 			}
 			else
 			{
+				//print filename with current 
+				//num of links in stdin
 				if(i == stbuf.st_nlink)
                 {
                     printf("%s\n", dirstr->d_name);
@@ -67,8 +80,17 @@ int main(int argc, char *argv[])
 			}
 
         }
+        //pointer get first catalog file link
         rewinddir(dir);
         printf("====================\n\n");
     }
+    
+    //close a directory
+    if(closedir(dir) == -1)
+    {
+		perror("closedir() ");
+		return errno;
+	}
+	
     return 0;
 }
